3260cb717d89ba727cd38f487eb608bf11c8facc
Greg Nowicki <nowicki.greg@gmail.com>
2014-03-16 21:56:26 +0000

    added category model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  has_many :articlec_ategories
+  has_many :article_categories
   has_many :categories, through: :article_categories
   
   validates :title, presence: true
