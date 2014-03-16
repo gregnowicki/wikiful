980cb9c332905a78c1399571afa6ae9c98a22d58
Greg Nowicki <nowicki.greg@gmail.com>
2014-03-16 21:42:00 +0000

    Added ArticleCategory model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316191627) do
+ActiveRecord::Schema.define(version: 20140316211401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: true do |t|
+    t.integer  "category_id"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
