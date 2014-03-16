84f7e4ff1e44634d9ddc78a1e387d83714bbfec2
Greg Nowicki <nowicki.greg@gmail.com>
2014-03-16 20:49:16 +0000

    Added Articles model and created associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :articlec_ategories
+  has_many :categories, through: :article_categories
+  
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :category, presence: true
 end
