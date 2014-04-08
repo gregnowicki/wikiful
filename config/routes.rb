e0fb6f1edea6ed23256e2bce4394a4d414002a4e
Greg Nowicki <nowicki.greg@gmail.com>
2014-04-08 23:49:07 +0000

    added users new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Wikiful::Application.routes.draw do
   resources :users
-
+  resources :articles
+  resources :categories
+  
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  resources :articles
-  resources :categories
+
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
